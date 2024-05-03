1621a0d1a8536ea35af55266c5bfd830e0bc9596
tungpt <phamthanhtungdcn@gmail.com>
2024-05-03 21:55:49 +0700

    #5 Leetcode 165 (Done)

--- src/leetcode/Leetcode_165.java
+++ src/leetcode/Leetcode_165.java
@@ -12,27 +12,27 @@ public class Leetcode_165 {
     public static int compareVersion(String version1, String version2) {
     	String[] v1 = version1.split("\\.");
     	String[] v2 = version2.split("\\.");
-    	String[] raw;
-    	
-    	if(v2.length > v1.length) {
-    		raw = v2;
-    		v2 = v1;
-    		v1 = raw;
+    	int dotCount = Math.abs(v2.length - v1.length);
+    	StringBuffer v2Raw = new StringBuffer(version2);
+    	StringBuffer v1Raw = new StringBuffer(version1);		
+
+    	if(v1.length > v2.length) {
+    		for(int i = 0; i < dotCount; i++) {
+    			v2Raw.append(".0");
+    		}
+    	} else if(v1.length < v2.length){
+    		for(int i = 0; i < dotCount; i++) {
+    			v1Raw.append(".0");
+    		}
     	}
+    	v1 = v1Raw.toString().split("\\.");
+    	v2 = v2Raw.toString().split("\\.");
     	
     	for(int i = 0; i < v1.length; i++) {
-    		if(i > v2.length -1) {
-        		if(Integer.parseInt(v1[i]) > 0) {
-        			return 1;
-        		} else if(Integer.parseInt(v1[i]) < 0){
-        			return -1;
-        		}
-    		} else {
-        		if(Integer.parseInt(v1[i]) > Integer.parseInt(v2[i])) {
-        			return 1;
-        		} else if(Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])){
-        			return -1;
-        		}
+    		if(Integer.parseInt(v1[i]) > Integer.parseInt(v2[i])) {
+    			return 1;
+    		} else if(Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])){
+    			return -1;
     		}
     		
 
